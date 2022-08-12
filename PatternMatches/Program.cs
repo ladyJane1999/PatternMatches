73bb626ec4b4bdefc97f9451049f9f3da7a93e7c
ladyJane1999 <69354433+ladyJane1999@users.noreply.github.com>
2022-08-12 15:17:39 +0300

    Update Program.cs

--- PatternMatches/Program.cs
+++ PatternMatches/Program.cs
@@ -1,4 +1,4 @@
-﻿using PatternMatches;
+using PatternMatches;
 
 // Task
 object obj = new Car(new Engine(105));
@@ -16,15 +16,15 @@ Console.WriteLine(isCarWith105PH);
 
 // Task 1
 static bool isLowPowerVechicle(object vechicle, bool isElectro)
-    {
+{
     vechicle = vechicle ?? throw new ArgumentNullException("null");
 
-    if (vechicle is SmartCar { isLowPowerVechicle: true } || vechicle is Car1 { engine1.power: < 100  } || vechicle is MotoSycele { engine1.power: < 100 }|| vechicle is Transport && isElectro)
+    if (vechicle is SmartCar { isLowPowerVechicle: true } || vechicle is Car1 { engine1.power: < 100 } || vechicle is MotoSycele { engine1.power: < 100 } || vechicle is Transport && isElectro)
     {
         Console.WriteLine(vechicle + " " + "Power is low");
         return true;
     }
-    else if(vechicle is SmartCar { isLowPowerVechicle: false } || vechicle is Car1 { engine1.power: > 100 } || vechicle is MotoSycele { engine1.power: > 100 })
+    else if (vechicle is SmartCar { isLowPowerVechicle: false } || vechicle is Car1 { engine1.power: > 100 } || vechicle is MotoSycele { engine1.power: > 100 })
     {
         Console.WriteLine(vechicle + " " + "Power ");
         return false;
@@ -33,20 +33,20 @@ static bool isLowPowerVechicle(object vechicle, bool isElectro)
     {
         throw new ArgumentException("Invalid object");
     }
- 
-    }
 
-    object obj2 = new MotoSycele(new Engine1(90));
-    object obj1 = new Car1(new Engine1(190));
-    object obj3 = new SmartCar(isLowPowerVechicle(obj1, true));
+}
 
-    var isPower1 = isLowPowerVechicle(obj1, false);
-    var isPower2 = isLowPowerVechicle(obj2, false);
+object obj2 = new MotoSycele(new Engine1(90));
+object obj1 = new Car1(new Engine1(190));
+object obj3 = new SmartCar(isLowPowerVechicle(obj1, true));
+
+var isPower1 = isLowPowerVechicle(obj1, false);
+var isPower2 = isLowPowerVechicle(obj2, false);
 try
 {
     var isLowPower = isLowPowerVechicle(42, false);
 }
 catch (ArgumentException ex)
 {
-    Console.Write(ex.Message); 
+    Console.Write(ex.Message);
 }
